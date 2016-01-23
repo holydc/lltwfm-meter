ac39ea5c9391d64868814b67edddfd6c121714c5
holydc <holydc@gmail.com>
2016-01-23 22:54:44 +0800

    落選崩潰中

---
+++ client.js
@@ -0,0 +1,202 @@
+function incrementor(x) {
+  --x;
+  return (x * x * x) + 1;
+}
+
+incrementor.duration = 3000;
+incrementor.interval = 30;
+incrementor.increment = incrementor.interval / incrementor.duration;
+
+function createImage(src, parent) {
+  var img = document.createElement('img');
+  parent.appendChild(img);
+  img.src = src;
+  return img;
+}
+
+function createSectionBackground(el, parent, front) {
+  var left = 0, top = 0, width = el.offsetWidth, height = el.offsetHeight;
+  while (el) {
+    left += el.offsetLeft;
+    top += el.offsetTop;
+    el = el.offsetParent;
+  }
+  
+  var div = document.createElement('div');
+  parent.appendChild(div);
+  div.style.position = 'absolute';
+  div.style.left = '' + left + 'px';
+  div.style.top = '' + top + 'px';
+  div.style.width = '' + width + 'px';
+  div.style.height = '' + height + 'px';
+  if (!front) {
+    div.style.zIndex = -1;
+  }
+  return div;
+}
+
+function createSection(title, data, parent) {
+  var type = title.match(/([^\d]+)\d/)[1];
+  var bar = document.createElement('div');
+  parent.appendChild(bar);
+  bar.className = [title, type, 'section'].join(' ');
+
+  var meter = document.createElement('div');
+  bar.appendChild(meter);
+  meter.className = [type, 'meter'].join(' ');
+  
+  var info = document.createElement('div');
+  bar.appendChild(info);
+  info.className = 'meter';
+  
+  var reserved = data.Afternoon[0] + data.Evening[0], total = data.Afternoon[1] + data.Evening[1];
+  var x = 0;
+  var t = setInterval(function () {
+    var y = incrementor(x);
+    if (y >= 1) {
+      clearInterval(t);
+      y = 1;
+    }
+    y *= reserved;
+    
+    var percentage = y * 100 / total;
+    meter.style.height = '' + percentage + '%';
+    info.innerHTML = '' + title + '<br>' + Math.floor(y) + '/' + total + '<br>' + (Math.floor(percentage * 100) / 100) + '%';
+    x += incrementor.increment;
+  }, incrementor.interval);
+}
+
+function draw(canvas, percentage) {
+  var x = canvas.width / 2, y = canvas.height / 2;
+  var lineWidth = 20;
+  var radius = x - (lineWidth / 2);
+  var context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.lineWidth = lineWidth;
+
+  context.beginPath();
+  context.arc(x, y, radius, 0, 2 * Math.PI);
+  context.strokeStyle = 'rgba(255, 255, 255, 0.3)';
+  context.stroke();
+  
+  context.beginPath();
+  context.arc(x, y, radius, 1.5 * Math.PI, (1.5 + (2 * percentage)) * Math.PI);
+  context.strokeStyle = 'white';
+  context.stroke();
+  
+  context.fillStyle = 'white';
+  context.textAlign = 'center';
+  context.font = '24px sans-serif';
+  context.fillText('' + (Math.floor(percentage * 10000) / 100) + '%', x, y);
+}
+
+function createDetail(title, data, parent) {
+  var className = [title, 'detail'].join(' ');
+  var detail = document.createElement('div');
+  parent.appendChild(detail);
+  detail.className = className;
+
+  var expanded = false;
+  var timers = [];
+  detail.addEventListener('click', function () {
+    if (!expanded) {
+      function onExpanded() {
+        function showInfo(title, data) {
+          var div = document.createElement('div');
+          detail.appendChild(div);
+
+          var canvas = document.createElement('canvas');
+          div.appendChild(canvas);
+          canvas.width = 200;
+          canvas.height = 200;
+
+          var span = document.createElement('span');
+          div.appendChild(span);
+
+          var x = 0;
+          var t = setInterval(function () {
+            var y = incrementor(x);
+            if (y >= 1) {
+              clearInterval(t);
+              y = 1;
+            }
+            y *= data[0];
+            draw(canvas, y / data[1]);
+            span.textContent = title + ': ' + Math.floor(y) + ' / ' + data[1];
+            x += incrementor.increment;
+          }, incrementor.interval);
+          timers.push(t);
+        }
+
+        showInfo('午場', data.Afternoon);
+        showInfo('晚場', data.Evening);
+      }
+
+      var transitionProperties = ['left', 'top', 'width', 'height'];
+      detail.className += ' expanded';
+      detail.addEventListener('transitionend', function _onTransitionEnd(event) {
+        var i = transitionProperties.indexOf(event.propertyName);
+        if (i >= 0) {
+          transitionProperties.splice(i, 1);
+        }
+        if (transitionProperties.length === 0) {
+          detail.removeEventListener('transitionend', _onTransitionEnd);
+          onExpanded();
+        }
+      });
+    } else {
+      for (var i = 0; i < timers.length; ++i) {
+        clearInterval(timers[i]);
+      }
+      timers = [];
+      detail.innerHTML = '';
+      detail.className = className;
+    }
+    expanded = !expanded;
+  });
+}
+
+function ready() {
+  var center = document.createElement('center');
+  document.body.appendChild(center);
+
+  createImage('event.jpg', center);
+  center.appendChild(document.createElement('br'));
+
+  var seats = createImage('seats.png', center);
+  center.appendChild(document.createElement('br'));
+  
+  var message = document.createElement('span');
+  center.appendChild(message);
+  message.style.color = 'black';
+  message.textContent = 'Loading...';
+
+  var sectionBackground = createSectionBackground(seats, document.body);
+  var sectionForeground = createSectionBackground(seats, document.body, true);
+
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState !== this.DONE) {
+      return;
+    }
+
+    var data = JSON.parse(this.response);
+    var reserved = 0, total = 0;
+    for (var k in data) {
+      if (data.hasOwnProperty(k)) {
+        var _data = data[k];
+        createSection(k, _data, sectionBackground);
+        createDetail(k, _data, sectionForeground);
+        reserved += (_data.Afternoon[0] + _data.Evening[0]);
+        total += (_data.Afternoon[1] + _data.Evening[1]);
+      }
+    }
+    message.textContent = 'Total: ' + reserved + ' / ' + total + ' (' + (Math.floor(reserved * 10000 / total) / 100) + ')';
+  };
+  xhr.open('GET', 'data');
+  xhr.send();
+}
+
+(function () {
+  document.addEventListener('DOMContentLoaded', ready);
+})();
---
+++ server.js
@@ -0,0 +1,121 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var parseUrl = require('url').parse;
+var path = require('path');
+var querystring = require('querystring');
+
+var moment = require('./moment');
+
+var port = process.env.port || 1337;
+
+var games = [
+  {title:'Afternoon',id:14223},
+  {title:'Evening',id:14224}
+];
+
+var sections = [
+  {title:'VIP1',id:11898}, {title:'VIP2',id:11899}, {title:'VIP3',id:11900}, {title:'VIP4',id:11918}, {title:'VIP5',id:11919},
+  {title:'S1',id:11901}, {title:'S2',id:11920}, {title:'S3',id:11902}, {title:'S4',id:11903}, {title:'S5',id:11904},
+  {title:'A1',id:11905}, {title:'A2',id:11906}, {title:'A3',id:11921}, {title:'A4',id:11922}, {title:'A5',id:11907}
+];
+
+var DATA_FILE = 'data.json';
+
+function getContentType(filename) {
+  switch (path.extname(filename).toLowerCase()) {
+    case '.js':
+      return 'application/javascript';
+    case '.gif':
+      return 'image/gif';
+    case '.jpg':
+      return 'image/jpeg';
+    case '.png':
+      return 'image/png';
+    case '.css':
+      return 'text/css';
+    case '.html':
+      return 'text/html';
+    default:
+      return 'text/plain';
+  }
+}
+
+function handleGet(url, response) {
+  var filename = path.basename(parseUrl(url).pathname);
+  if (!filename) {
+    filename = 'index.html';
+  }
+  fs.readFile(filename, function (error, data) {
+    response.writeHead(200, {
+      'Content-Type': getContentType(filename)
+    });
+    response.end(data);
+  });
+}
+
+function outputDataFile(response) {
+  fs.readFile(DATA_FILE, function (error, data) {
+    response.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    response.end(data);
+  });
+}
+
+function update(response) {
+  var requested = 0, responded = 0;
+  var result = {};
+
+  function query(game, section) {
+    ++requested;
+
+    var url = 'https://www.famiticket.com.tw/FWT/FWT0040.aspx?activitycode=1512Z04005&chose_type=false&ticket_total=1&game_id=' + game.id + '&section_id=' + section.id;
+    https.get(url, function (res) {
+      var html = '';
+      res.on('data', function (data) {
+        html += data;
+      });
+      res.on('end', function () {
+        if (!result[section.title]) {
+          result[section.title] = {};
+        }
+        result[section.title][game.title] = [html.match(/src="image\/icon\/icon_4.gif"/g).length, html.match(/src="image\/icon\/icon_[47].gif"/g).length];
+
+        ++responded;
+        if (responded === requested) {
+          fs.writeFile(DATA_FILE, JSON.stringify(result), function (error) {
+            outputDataFile(response);
+          });
+        }
+      });
+    });
+  }
+
+  for (var i = 0; i < games.length; ++i) {
+    for (var j = 0; j < sections.length; ++j) {
+      query(games[i], sections[j]);
+    }
+  }
+}
+
+function handleData(response) {
+  fs.stat(DATA_FILE, function (error, stats) {
+    if (error || (moment().diff(moment(stats.mtime), 'hours') > 720)) {
+      update(response);
+    } else {
+      outputDataFile(response);
+    }
+  });
+}
+
+http.createServer(function (request, response) {
+  switch (request.url) {
+    case '/data':
+      handleData(response);
+      break;
+    default:
+      handleGet(request.url, response);
+      break;
+  }
+}).listen(port);
